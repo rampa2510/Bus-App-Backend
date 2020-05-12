fd2aebe0300a77fe0db1798476dbebad5a7da003
iamram2510 <iamram2510@ieee.org>
2020-05-13 01:18:43 +0530

    Done

--- Controllers/admin.js
+++ Controllers/admin.js
@@ -42,7 +42,7 @@ module.exports.addBus = async (req,res)=>{
     const db = await get();
     const doesBusNoExist = await findOne({number:req.body.number},'bus',db);
     if(doesBusNoExist) return res.status(400).send({message:"Bus number already exists"});
-    await insertOne({number:req.body.number,start:req.body.start,end:req.body.end,passengers:[],price:req.body.price},'bus',db)
+    await insertOne({number:req.body.number,start:req.body.start,end:req.body.end,passengers:[],price:req.body.price,time:req.body.time},'bus',db)
     res.status(200).send({message:'Ok'})
   } catch (err) {
     console.log(err)
