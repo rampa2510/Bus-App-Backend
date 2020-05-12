786ca20f2786ad30c5c80f143bd6aead0e264bb8
iamram2510 <iamram2510@ieee.org>
2020-05-13 01:06:32 +0530

    Changes

--- Controllers/driver.js
+++ Controllers/driver.js
@@ -4,7 +4,7 @@ module.exports.updateBus =async (req,res)=>{
   try {
     const db = await get()
     await updateOne({number:req.body.number},
-      {$set:{driver:req.body.username,time:req.body.time}}
+      {$set:{driver:req.body.username}}
       ,'bus',db);
     res.status(200).send({message:'Ok'})
   } catch (error) {
@@ -15,6 +15,11 @@ module.exports.updateBus =async (req,res)=>{
 module.exports.updateBusRoute =async (req,res)=>{
   try {
     const db = await get()
+    const data = await findOne({number:req.body.number},'bus',db);
+    if(data.end===req.body.reached){
+      await updateOne({number:req.body.number},{$set:{reached:req.body.reached,end:true}},'bus',db);
+      return res.status(200).send({message:'Ok'})
+    }
     await updateOne({number:req.body.number},{$set:{reached:req.body.reached}},'bus',db);
     res.status(200).send({message:'Ok'})
   } catch (error) {
@@ -26,8 +31,8 @@ module.exports.updateBusRoute =async (req,res)=>{
 module.exports.getVacantBus = async (req,res)=>{
   try {
     const db = await get()
-    const checkIfBusIsDriving = await findOne({driver:req.body.username},'bus',db)
-    if(checkIfBusIsDriving && checkIfBusIsDriving.reached!==checkIfBusIsDriving.end){
+    const checkIfBusIsDriving = await findOne({$and:[{driver:req.body.username},{end:{$exists:false}}]},'bus',db)
+    if(checkIfBusIsDriving){
       // if(checkIfBusIsDriving.)
         return res.status(200).send({message:"Ok",data:{...checkIfBusIsDriving,driverBusy:true}})
     }
